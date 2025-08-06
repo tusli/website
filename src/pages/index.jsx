fc91668b72c5f132ec8deef63f0f4a074d5bf9a0
tusli <tusli.travel@gmail.com>
2025-08-06 14:45:44 +0300

    Fix hyphenated import names and add utility function

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,37 +24,37 @@ import OfferDetails from "./OfferDetails.jsx";
 
 import NotFound from "./NotFound.jsx";
 
-import family-trips from "./family-trips";
+import familyTrips from "./family-trips";
 
-import request-a-quote from "./request-a-quote";
+import requestAQuote from "./request-a-quote";
 
-import romantic-vacations from "./romantic-vacations";
+import romanticVacations from "./romantic-vacations";
 
-import kosher-vacations from "./kosher-vacations";
+import kosherVacations from "./kosher-vacations";
 
 import Destinations from "./Destinations.jsx";
 
 import DestinationDetails from "./DestinationDetails.jsx";
 
-import honeymoon-trips from "./honeymoon-trips";
+import honeymoonTrips from "./honeymoon-trips";
 
-import backpacker-trips from "./backpacker-trips";
+import backpackerTrips from "./backpacker-trips";
 
-import luxury-vacations from "./luxury-vacations";
+import luxuryVacations from "./luxury-vacations";
 
-import ski-vacations from "./ski-vacations";
+import skiVacations from "./ski-vacations";
 
-import group-tours from "./group-tours";
+import groupTours from "./group-tours";
 
-import last-minute-deals from "./last-minute-deals";
+import lastMinuteDeals from "./last-minute-deals";
 
-import business-travel from "./business-travel";
+import businessTravel from "./business-travel";
 
 import cruises from "./cruises.jsx";
 
-import travel-insurance from "./travel-insurance";
+import travelInsurance from "./travel-insurance";
 
-import custom-trip-builder from "./custom-trip-builder";
+import customTripBuilder from "./custom-trip-builder";
 
 import greece from "./greece.jsx";
 
@@ -66,11 +66,12 @@ import london from "./london.jsx";
 
 import paris from "./paris.jsx";
 
-import sports-packages from "./sports-packages";
+import sportsPackages from "./sports-packages";
 
-import concert-packages from "./concert-packages";
+import concertPackages from "./concert-packages";
 
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { hyphenToCamelCase } from '../utils/index.ts';
 
 const PAGES = {
     
@@ -98,37 +99,37 @@ const PAGES = {
     
     NotFound: NotFound,
     
-    family-trips: family-trips,
+    familyTrips: familyTrips,
     
-    request-a-quote: request-a-quote,
+    requestAQuote: requestAQuote,
     
-    romantic-vacations: romantic-vacations,
+    romanticVacations: romanticVacations,
     
-    kosher-vacations: kosher-vacations,
+    kosherVacations: kosherVacations,
     
     Destinations: Destinations,
     
     DestinationDetails: DestinationDetails,
     
-    honeymoon-trips: honeymoon-trips,
+    honeymoonTrips: honeymoonTrips,
     
-    backpacker-trips: backpacker-trips,
+    backpackerTrips: backpackerTrips,
     
-    luxury-vacations: luxury-vacations,
+    luxuryVacations: luxuryVacations,
     
-    ski-vacations: ski-vacations,
+    skiVacations: skiVacations,
     
-    group-tours: group-tours,
+    groupTours: groupTours,
     
-    last-minute-deals: last-minute-deals,
+    lastMinuteDeals: lastMinuteDeals,
     
-    business-travel: business-travel,
+    businessTravel: businessTravel,
     
     cruises: cruises,
     
-    travel-insurance: travel-insurance,
+    travelInsurance: travelInsurance,
     
-    custom-trip-builder: custom-trip-builder,
+    customTripBuilder: customTripBuilder,
     
     greece: greece,
     
@@ -140,9 +141,9 @@ const PAGES = {
     
     paris: paris,
     
-    sports-packages: sports-packages,
+    sportsPackages: sportsPackages,
     
-    concert-packages: concert-packages,
+    concertPackages: concertPackages,
     
 }
 
@@ -155,7 +156,8 @@ function _getCurrentPage(url) {
         urlLastPart = urlLastPart.split('?')[0];
     }
 
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
+    const camelCaseUrlPart = hyphenToCamelCase(urlLastPart);
+    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === camelCaseUrlPart.toLowerCase());
     return pageName || Object.keys(PAGES)[0];
 }
 
@@ -195,37 +197,37 @@ function PagesContent() {
                 
                 <Route path="/NotFound" element={<NotFound />} />
                 
-                <Route path="/family-trips" element={<family-trips />} />
+                <Route path="/family-trips" element={<familyTrips />} />
                 
-                <Route path="/request-a-quote" element={<request-a-quote />} />
+                <Route path="/request-a-quote" element={<requestAQuote />} />
                 
-                <Route path="/romantic-vacations" element={<romantic-vacations />} />
+                <Route path="/romantic-vacations" element={<romanticVacations />} />
                 
-                <Route path="/kosher-vacations" element={<kosher-vacations />} />
+                <Route path="/kosher-vacations" element={<kosherVacations />} />
                 
                 <Route path="/Destinations" element={<Destinations />} />
                 
                 <Route path="/DestinationDetails" element={<DestinationDetails />} />
                 
-                <Route path="/honeymoon-trips" element={<honeymoon-trips />} />
+                <Route path="/honeymoon-trips" element={<honeymoonTrips />} />
                 
-                <Route path="/backpacker-trips" element={<backpacker-trips />} />
+                <Route path="/backpacker-trips" element={<backpackerTrips />} />
                 
-                <Route path="/luxury-vacations" element={<luxury-vacations />} />
+                <Route path="/luxury-vacations" element={<luxuryVacations />} />
                 
-                <Route path="/ski-vacations" element={<ski-vacations />} />
+                <Route path="/ski-vacations" element={<skiVacations />} />
                 
-                <Route path="/group-tours" element={<group-tours />} />
+                <Route path="/group-tours" element={<groupTours />} />
                 
-                <Route path="/last-minute-deals" element={<last-minute-deals />} />
+                <Route path="/last-minute-deals" element={<lastMinuteDeals />} />
                 
-                <Route path="/business-travel" element={<business-travel />} />
+                <Route path="/business-travel" element={<businessTravel />} />
                 
                 <Route path="/cruises" element={<cruises />} />
                 
-                <Route path="/travel-insurance" element={<travel-insurance />} />
+                <Route path="/travel-insurance" element={<travelInsurance />} />
                 
-                <Route path="/custom-trip-builder" element={<custom-trip-builder />} />
+                <Route path="/custom-trip-builder" element={<customTripBuilder />} />
                 
                 <Route path="/greece" element={<greece />} />
                 
@@ -237,9 +239,9 @@ function PagesContent() {
                 
                 <Route path="/paris" element={<paris />} />
                 
-                <Route path="/sports-packages" element={<sports-packages />} />
+                <Route path="/sports-packages" element={<sportsPackages />} />
                 
-                <Route path="/concert-packages" element={<concert-packages />} />
+                <Route path="/concert-packages" element={<concertPackages />} />
                 
             </Routes>
         </Layout>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,6 @@
 export function createPageUrl(pageName: string) {
     return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
+export function hyphenToCamelCase(str: string): string {
+    return str.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase());
+}
